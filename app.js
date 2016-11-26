d925ffed6393a687182e9820a645c95d3398fb6a
sarmad zaki <sarmad zaki>
2016-11-26 12:09:21 +0500

    todo

---
+++ app.js
@@ -0,0 +1,15 @@
+
+function todoList() {
+	
+	var item    = document.getElementById('todoInput').value;
+	
+	var text    = document.createTextNode(item);
+	
+
+	var newItem = document.createElement('li');
+	 
+	newItem.appendChild(text);
+		
+	
+	document.getElementById('todoList list-group').appendChild(newItem);
+}
